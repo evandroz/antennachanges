13f35bd2a236dfa5d316f752c1ae224540a3e5e5
evandroz <droz.evan@gmail.com>
2021-02-21 11:33:42 -0800

    edits on datavis 02-21
    progression on datavis, transfer to laptop

--- Matlab/Matlab/orbitvis/data_visualization.m
+++ Matlab/Matlab/orbitvis/data_visualization.m
@@ -13,14 +13,14 @@ filenames = dir('*.csv');
 
 for i = 1:numfiles
     assetname = (filenames(i).name(8:end-4));
-    asset.(assetname) = readtable(filenames(i).name);
-    columns = width(asset.(assetname))
-    for i = 1:columns-1
+    asset.(assetname) = readtable(filenames(i).name, 'Delimiter',{',', ';'}, 'Whitespace' ,'[]');
+    columns = width(asset.(assetname));
+    for j = 1:columns-1
        nexttile
-       plot(asset.(assetname).(1),asset.(assetname).(i+1))
-       title(assetname,'vs. time')
+       plot(asset.(assetname).(1),(asset.(assetname).(j+1)))
+       title(asset.(assetname).Properties.VariableNames(j+1),'vs. time', 'Interpreter', 'none')
        xlabel('time')
-%        ylabel(asset.(assetname).Properties.VariableNames.(i+1))
+       ylabel(asset.(assetname).Properties.VariableNames(j+1))%, 'Interpreter', 'none')
     end
     
     
