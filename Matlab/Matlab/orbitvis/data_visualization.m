435b5c038203d3940c7bc87487c0ad3acca10f68
evandroz <droz.evan@gmail.com>
2021-03-02 09:26:59 -0800

    Update data_visualization.m

--- Matlab/Matlab/orbitvis/data_visualization.m
+++ Matlab/Matlab/orbitvis/data_visualization.m
@@ -15,12 +15,18 @@ for i = 1:numfiles
     assetname = (filenames(i).name(8:end-4));
     asset.(assetname) = readtable(filenames(i).name, 'Delimiter',{',', ';'}, 'Whitespace' ,'[]');
     columns = width(asset.(assetname));
+    hold off
+    nexttile
     for j = 1:columns-1
-       nexttile
+       if isequal (class(asset.(assetname).(j+1)), ('cell'))
+           asset.(assetname).(j+1) = str2double(asset.(assetname).(j+1));
+       end
        plot(asset.(assetname).(1),(asset.(assetname).(j+1)))
-       title(asset.(assetname).Properties.VariableNames(j+1),'vs. time', 'Interpreter', 'none')
+       title(assetname, 'vs. time', 'Interpreter', 'none')%asset.(assetname).Properties.VariableNames(j+1),'vs. time', 'Interpreter', 'none')
        xlabel('time')
-       ylabel(asset.(assetname).Properties.VariableNames(j+1))%, 'Interpreter', 'none')
+       ylabel(assetname)%asset.(assetname).Properties.VariableNames(j+1))%, 'Interpreter', 'none')
+       hold on
+       
     end
     
     
