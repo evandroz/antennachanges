2f9b4d92f25264794ea5b2ce1b66bd2fa715363e
evandroz <droz.evan@gmail.com>
2021-03-13 10:26:25 -0800

    update 3/13/21 added notes and updated ttr
    transferring to laptop with consolidated notes for locations of files to adjust initial conditions.  todays goal is figuring out further data info that adjusts based on the scheduling and what is the limitation on the performance

--- Matlab/Matlab/orbitvis/data_visualization.m
+++ Matlab/Matlab/orbitvis/data_visualization.m
@@ -10,10 +10,15 @@ clc
 filenames = dir('*.csv');
 [numfiles, ~] = size(filenames);
     tiledlayout('flow')
-
+figure(1)
 for i = 1:numfiles
     assetname = (filenames(i).name(8:end-4));
+    if isequal(assetname(end),'_')
+        assetname = assetname(1:end-1);
+%         disp(assetname)
+    end
     asset.(assetname) = readtable(filenames(i).name, 'Delimiter',{',', ';'}, 'Whitespace' ,'[]');
+    
     columns = width(asset.(assetname));
     hold off
     nexttile
@@ -22,13 +27,13 @@ for i = 1:numfiles
            asset.(assetname).(j+1) = str2double(asset.(assetname).(j+1));
        end
        plot(asset.(assetname).(1),(asset.(assetname).(j+1)))
-       title(assetname, 'vs. time', 'Interpreter', 'none')%asset.(assetname).Properties.VariableNames(j+1),'vs. time', 'Interpreter', 'none')
-       xlabel('time')
-       ylabel(assetname)%asset.(assetname).Properties.VariableNames(j+1))%, 'Interpreter', 'none')
+
        hold on
        
     end
-    
-    
+       title(assetname, 'vs. time', 'Interpreter', 'none')%asset.(assetname).Properties.VariableNames(j+1),'vs. time', 'Interpreter', 'none')
+       xlabel('time')
+       ylabel(assetname)%asset.(assetname).Properties.VariableNames(j+1))%, 'Interpreter', 'none')
+       legend
 end
-    
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
